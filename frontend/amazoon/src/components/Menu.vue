c68826c013e206888d6d51ae84676b40ccbcd5aa
leokorrr <leokorrr@gmail.com>
2019-05-22 15:34:06 +0200

    Gogle and Amazoon added

---
+++ frontend/amazoon/src/components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-light menu">
+        <a class="navbar-brand" href="#">Amazoon Careers</a>
+    </nav>
+</template>
+
+<style lang="scss">
+    $color-main: #185682;
+
+    .menu{
+        background-color: $color-main;
+
+        & .navbar-brand {
+            color: white;
+            transition: .2s;
+
+            &:hover{
+                color: darken($color: white, $amount: 15%)
+            }
+        }
+    }
+</style>
