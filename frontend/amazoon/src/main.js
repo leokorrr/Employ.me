32b7096a9a3d3d94bb387cd702ecc92885b1f439
Leon Koczetkow <lkachatkou@kaliop.com>
2019-06-12 13:33:47 +0200

    vue router added

--- frontend/amazoon/src/main.js
+++ frontend/amazoon/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router.js';
+import VueRouter from 'vue-router';
+import Job from './components/Job.vue';
+import Jobs from './components/Jobs.vue';
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/job',
+    component: Job,
+  },
+  {
+    path: '/',
+    component: Jobs
+  }
+];
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   router,
